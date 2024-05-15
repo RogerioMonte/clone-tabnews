3b7875262c97de7208a5be23cda50c6b42b32a9f
Rogério Monte <104043314+RogerioMonte@users.noreply.github.com>
2024-05-15 00:59:02 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            <h1>Desenvolva o que você queser!!!</h1>
+        </div>
+    ); 
+}
+export default Home;

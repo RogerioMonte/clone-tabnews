c4e25aec0ad90d6bc9af55dc19c0c39dc7b38a88
Rogério Monte <104043314+RogerioMonte@users.noreply.github.com>
2024-05-15 21:34:42 +0000

    Desenvolva o que você queser!

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-            <h1>Desenvolva o que você queser!!!</h1>
+            <h1>Desenvolva o que você queser!</h1>
         </div>
     ); 
 }
